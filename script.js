c58794a9ca73d6706fd72561b581b96f2a970407
James <131582155+Namra14@users.noreply.github.com>
2024-08-26 23:43:32 +0800

    dsadasdsa

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ const $prev = document.querySelector(".prev");
 $next.addEventListener("click", () => {
         const items = document.querySelectorAll(".item");
         document.querySelector(".slide").appendChild(items[0]);
-    },
-
-);
+    });
 
 $prev.addEventListener("click", () => {
         const items = document.querySelectorAll(".item");
         document.querySelector(".slide").prepend(items[items.length - 1]);
-    },
-);
+    });
+
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('whole-nav');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#7ACAD2'; // Change color when scrolled
+    } else {
+        navbar.style.backgroundColor = 'transparent'; // Transparent when at top
+    }
+});
+
