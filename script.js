d22d863f68ae48a095a7c5cfb430f86000159ebd
James <131582155+Namra14@users.noreply.github.com>
2024-08-29 00:11:00 +0800

    responsiveness navbar

--- script.js
+++ script.js
@@ -20,3 +20,10 @@ window.addEventListener('scroll', function() {
     }
 });
 
+const menuIcon = document.querySelector(".menu-icon");
+const nav = document.querySelector(".nav");
+
+menuIcon.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
